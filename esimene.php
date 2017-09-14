fb702ea6d9147ee57e64358eb4a2181b11d74229
Igor Filippov <igorfilippov474@users.noreply.github.com>
2017-09-14 10:47:04 +0300

    lisatud on esimene php jupp

---
+++ esimene.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset=”utf-8”>
+	<title>Igor Filippov</title>
+</head>
+<body>
+	<h1>BIO</h1>
+	<p>See veebileht on loodud oppetoo raames ning ei sisalda mingisugust tosiseltvoetavat sisu! </p>
+	<p> kodutoo TEST </p>
+	<p> Minu nimi on Igor Filippov olen TLU tudeng ÜÜÜü	  </p>
+	<?php
+		echo "Algas PHP oppimine.";
+	?>
+
+
+
+</body>
+</html>
