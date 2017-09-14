d583f014ce3c0118f9733e6f73f8bed4449706ab
Igor Filippov <igorfilippov474@users.noreply.github.com>
2017-09-14 11:32:41 +0300

    lisatud muutujad ning IF lause

--- esimene.php
+++ esimene.php
@@ -1,3 +1,23 @@
+<?php
+	//muutujad
+	$myName = "Igor";
+	$myFamilyName= "Filippov";
+	
+	//hindan paeva osa
+	$hourNow = date("H");
+	$partOfDay = "";
+	if ($hourNow < 8) {
+	$partOfday= "varajane hommik";
+	}
+	if ($hourNow >= 8 and $hourNow <16){
+		$partOfDay = "koolipaev";
+	}
+	if ($hourNow > 16 ) {
+		$partOfDay= "vaba aeg";
+	}
+	echo $partOfDay;
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,12 +25,15 @@
 	<title>Igor Filippov</title>
 </head>
 <body>
-	<h1>BIO</h1>
+	<h1><?php echo $myName ." " .$myFamilyName; ?> </h1>
 	<p>See veebileht on loodud oppetoo raames ning ei sisalda mingisugust tosiseltvoetavat sisu! </p>
 	<p> kodutoo TEST </p>
 	<p> Minu nimi on Igor Filippov olen TLU tudeng ÜÜÜü	  </p>
 	<?php
-		echo "Algas PHP oppimine.";
+		echo "<p>Algas PHP oppimine.</p>";
+		echo "<p>Tana on ";
+		echo date ("d.m.Y") .", kell oli lehe avamise hetkel " .date("H:i:s");
+		echo ", hetkel on " .$partOfDay. ".</p>";
 	?>
 
 
